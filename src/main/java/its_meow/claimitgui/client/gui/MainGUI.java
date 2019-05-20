68a5871f8168c290abee175a4733bb0716f8ca52
itsmeow <mods.itsmeow@gmail.com>
2019-05-20 09:41:56 -0400

    Get rid of weird list thing

--- src/main/java/its_meow/claimitgui/client/gui/MainGUI.java
+++ src/main/java/its_meow/claimitgui/client/gui/MainGUI.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.ScaledResolution;
 
 public class MainGUI extends GuiScreen {
 
     public static final int CLAIM_LIST_ID = 512325;
     public GuiButton claimListButton = null;
     public OwnedClaimListGUI claimList = null;
-    public boolean drawClaimList = false;
 
     public MainGUI() {
         this.mc = Minecraft.getMinecraft();
@@ -20,18 +18,14 @@ public class MainGUI extends GuiScreen {
 
     @Override
     public void initGui() {
-        ScaledResolution res = new ScaledResolution(this.mc);
         claimListButton = new GuiButton(CLAIM_LIST_ID, 0, 0, 50, 20, "Claim List");
         this.addButton(claimListButton);
-        claimList = new OwnedClaimListGUI(Minecraft.getMinecraft(), this.mc.displayWidth, this.mc.displayHeight, this.mc.displayHeight, 0, 1);
+        claimList = new OwnedClaimListGUI();
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.drawDefaultBackground();
-        if(drawClaimList) {
-            claimList.drawScreen(mouseX, mouseY, partialTicks);
-        }
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -43,7 +37,7 @@ public class MainGUI extends GuiScreen {
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
         if(button.id == CLAIM_LIST_ID && button == claimListButton) {
-            this.drawClaimList = !this.drawClaimList;
+            Minecraft.getMinecraft().displayGuiScreen(claimList);
         }
     }
 
--- src/main/java/its_meow/claimitgui/client/gui/OwnedClaimListGUI.java
+++ src/main/java/its_meow/claimitgui/client/gui/OwnedClaimListGUI.java
@@ -1,48 +1,25 @@
 package its_meow.claimitgui.client.gui;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiListExtended;
+import java.io.IOException;
 
-public class OwnedClaimListGUI extends GuiListExtended {
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
 
-    public OwnedClaimListGUI(Minecraft mcIn, int widthIn, int heightIn, int topIn, int bottomIn, int slotHeightIn) {
-        super(mcIn, widthIn, heightIn, topIn, bottomIn, slotHeightIn);
-    }
-    
-    public static ClaimListEntry entry = new ClaimListEntry();
-    
+public class OwnedClaimListGUI extends GuiScreen {
+
     @Override
-    public IGuiListEntry getListEntry(int index) {
-        return new ClaimListEntry();
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
     }
 
     @Override
-    protected int getSize() {
-        return 10;
+    protected void actionPerformed(GuiButton button) throws IOException {
+
     }
-    
-    public static class ClaimListEntry implements IGuiListEntry {
 
-        @Override
-        public void updatePosition(int slotIndex, int x, int y, float partialTicks) {
-            
-        }
+    @Override
+    public void initGui() {
 
-        @Override
-        public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, int mouseX, int mouseY, boolean isSelected, float partialTicks) {
-            Minecraft.getMinecraft().fontRenderer.drawString("x " + x, x + 100, y + 100, 0);
-        }
-
-        @Override
-        public boolean mousePressed(int slotIndex, int mouseX, int mouseY, int mouseEvent, int relativeX, int relativeY) {
-            return false;
-        }
-
-        @Override
-        public void mouseReleased(int slotIndex, int x, int y, int mouseEvent, int relativeX, int relativeY) {
-            
-        }
-        
     }
 
 }
