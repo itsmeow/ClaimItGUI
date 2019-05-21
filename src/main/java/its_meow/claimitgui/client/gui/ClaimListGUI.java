56ee6df8a4910c7cfec2fd5af4b0ef6763710831
itsmeow <mods.itsmeow@gmail.com>
2019-05-21 10:00:10 -0400

    Tweak background and colors

--- src/main/java/its_meow/claimitgui/client/gui/ClaimListGUI.java
+++ src/main/java/its_meow/claimitgui/client/gui/ClaimListGUI.java
@@ -10,18 +10,36 @@ import net.minecraft.client.gui.GuiScreen;
 
 public class ClaimListGUI extends GuiScreen {
     
+    
+    public static final int TITLE_BOX_COLOR = 0xff727272;
+    public static final int MAIN_BOX_COLOR = 0xff828282;
+    public static final int BACKGROUND_COLOR = 0xff424242;
+    public static final int BACKGROUND_COLOR_2 = 0xff121212;
+    public static final int MAIN_BACKGROUND_COLOR = 0xff626262;
+    public static final int MAIN_BACKGROUND_COLOR_2 = 0xff424242;
     public GuiScrollClaimPanel panel;
     public int panelWidth = 10;
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        this.drawBackground(0);
+        drawGradientRect(0, 0, width, height, MAIN_BACKGROUND_COLOR, MAIN_BACKGROUND_COLOR_2);
         this.panel.drawScreen(mouseX, mouseY, partialTicks);
+        drawVerticalLine(panelWidth, -1, height, 0xff2d2d2d);
+        drawVerticalLine(panelWidth + 1, -1, height, 0xff2d2d2d);
         this.drawString(mc.fontRenderer, "Claims List", 10, 10, 0xeeee00);
         ClaimArea claim = panel.getSelectedItem();
+        int xOff = 5;
+        int yOff = 3;
         if(claim != null) {
-            drawRect(panelWidth + 3, 25, width - 3, height - 3, 0xff727272);
-            drawRect(panelWidth + 3, 25, width - 3, (height / 4), 0xff828282);
+            drawRect(panelWidth + xOff, yOff, width - xOff, height - yOff, TITLE_BOX_COLOR);
+            drawRect(panelWidth + xOff, yOff, width - xOff, height / 4, MAIN_BOX_COLOR);
+            drawHorizontalLine(panelWidth + xOff, width - xOff, yOff, 0xff141414);
+            drawHorizontalLine(panelWidth + xOff, width - xOff, height / 4, 0xff141414);
+            drawHorizontalLine(panelWidth + xOff, width - xOff, height - yOff, 0xff141414);
+            drawVerticalLine(panelWidth + xOff, yOff, height - yOff, 0xff141414);
+            drawVerticalLine(width - xOff, yOff, height - yOff, 0xff141414);
+        } else {
+            this.drawCenteredString(mc.fontRenderer, "No claim selected.", (width - (width / 3)), 15, 0xe3e3e3);
         }
     }
 
@@ -33,7 +51,7 @@ public class ClaimListGUI extends GuiScreen {
     @Override
     public void initGui() {
         panelWidth = (int) (this.width / 3F);
-        panel = new GuiScrollClaimPanel(panelWidth, this.height, 25, this.height);
+        panel = new GuiScrollClaimPanel(panelWidth, this.height, 25, this.height, BACKGROUND_COLOR, BACKGROUND_COLOR_2);
         for(int i = 0; i < 50; i++) {
             panel.addItem(randClaim());
         }
--- src/main/java/its_meow/claimitgui/client/gui/objects/GuiScrollClaimPanel.java
+++ src/main/java/its_meow/claimitgui/client/gui/objects/GuiScrollClaimPanel.java
@@ -2,11 +2,53 @@ package its_meow.claimitgui.client.gui.objects;
 
 import its_meow.claimit.api.claim.ClaimArea;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class GuiScrollClaimPanel extends GuiScrollPanel<ClaimArea> {
 
-    public GuiScrollClaimPanel(int width, int height, int topIn, int bottomIn) {
+    protected final int backgroundColor;
+    protected final int backgroundColor2;
+
+    public GuiScrollClaimPanel(int width, int height, int topIn, int bottomIn, int backgroundColor, int backgroundColor2) {
         super(width, height, topIn, bottomIn, Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT + 2);
+        this.backgroundColor = backgroundColor;
+        this.backgroundColor2 = backgroundColor2;
+    }
+
+    @Override
+    protected void drawBackground() {
+        drawGradientRect(this.left, this.top, this.right, this.bottom, this.backgroundColor, this.backgroundColor2);
+    }
+    
+    public static void drawGradientRect(int left, int top, int right, int bottom, int startColor, int endColor) {
+        float f = (float)(startColor >> 24 & 255) / 255.0F;
+        float f1 = (float)(startColor >> 16 & 255) / 255.0F;
+        float f2 = (float)(startColor >> 8 & 255) / 255.0F;
+        float f3 = (float)(startColor & 255) / 255.0F;
+        float f4 = (float)(endColor >> 24 & 255) / 255.0F;
+        float f5 = (float)(endColor >> 16 & 255) / 255.0F;
+        float f6 = (float)(endColor >> 8 & 255) / 255.0F;
+        float f7 = (float)(endColor & 255) / 255.0F;
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.shadeModel(7425);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        bufferbuilder.pos((double)right, (double)top, 0).color(f1, f2, f3, f).endVertex();
+        bufferbuilder.pos((double)left, (double)top, 0).color(f1, f2, f3, f).endVertex();
+        bufferbuilder.pos((double)left, (double)bottom, 0).color(f5, f6, f7, f4).endVertex();
+        bufferbuilder.pos((double)right, (double)bottom, 0).color(f5, f6, f7, f4).endVertex();
+        tessellator.draw();
+        GlStateManager.shadeModel(7424);
+        GlStateManager.disableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableTexture2D();
     }
 
     @Override
