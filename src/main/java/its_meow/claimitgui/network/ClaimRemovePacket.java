2c219bada2874c7ee491ea135b9b8c9981b921b8
itsmeow <mods.itsmeow@gmail.com>
2019-05-22 10:55:33 -0400

    Add packet classes for claim tracking

---
+++ src/main/java/its_meow/claimitgui/network/ClaimRemovePacket.java
@@ -0,0 +1,51 @@
+package its_meow.claimitgui.network;
+
+import com.google.common.base.Charsets;
+
+import io.netty.buffer.ByteBuf;
+import its_meow.claimit.api.claim.ClaimArea;
+import its_meow.claimit.api.claim.ClaimManager;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class ClaimRemovePacket implements IMessage {
+    
+    public String trueName;
+    
+    public ClaimRemovePacket() {}
+    
+    public ClaimRemovePacket(ClaimArea claim) {
+        trueName = claim.getTrueViewName();
+    }
+    
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        int l = buf.readInt();
+        trueName = String.valueOf(buf.readCharSequence(l, Charsets.UTF_8));
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        buf.writeInt(trueName.length());
+        buf.writeCharSequence(trueName, Charsets.UTF_8);
+    }
+    
+    public static class Handler implements IMessageHandler<ClaimRemovePacket, IMessage> {
+
+        @Override
+        public IMessage onMessage(ClaimRemovePacket message, MessageContext ctx) {
+            if(ctx.side != Side.CLIENT) {
+                return null;
+            }
+            ClaimArea claim = ClaimManager.getManager().getClaimByTrueName(message.trueName);
+            if(claim != null) {
+                ClaimManager.getManager().deleteClaim(claim);
+            }
+            return null;
+        }
+        
+    }
+    
+}
