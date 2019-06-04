0ca66c68ae1ffe42344d5ca4291c85f827599696
itsmeow <mods.itsmeow@gmail.com>
2019-06-04 08:29:37 -0400

    Seperate into two panels

--- src/main/java/its_meow/claimitgui/client/gui/ClaimListGUI.java
+++ src/main/java/its_meow/claimitgui/client/gui/ClaimListGUI.java
@@ -42,7 +42,6 @@ public class ClaimListGUI extends GuiScreen {
 
     // Buttons
     public GuiButton refreshButton;
-    public static final int LOCATION_TAB_BUTTON_ID = 0;
     public static final int PERMISSION_TAB_BUTTON_ID = 1;
     public static final int GROUP_TAB_BUTTON_ID = 2;
 
@@ -58,58 +57,86 @@ public class ClaimListGUI extends GuiScreen {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        // Draw background and panel
         this.drawDefaultBackground();
         this.panel.drawScreen(mouseX, mouseY, partialTicks);
+        // Draw top layer of selection panel
         this.drawString(mc.fontRenderer, "Claims", 10, 10, 0xeeee00);
         refreshButton.drawButton(mc, mouseX, mouseY, partialTicks);
+        // Get claim and start claim-based renders
         ClaimArea claim = panel.getSelectedItem();
         if(claim != null) {
-            // draw background
+
+            // Draw backgrounds
+
+            // Initialize positioning
             int xOff = 5;
             int yOff = 3;
+            int mainPanelRight = (int) ((width * 0.66666F) - xOff);
             String title = "Claim: " + claim.getDisplayedViewName();
             int titleWidth = (int) (mc.fontRenderer.getStringWidth(title) * 1.5F);
             int topBarBottom = (int) (yOff + 8F + (mc.fontRenderer.FONT_HEIGHT * 1.5F));
-            int titleBarX = (int) (panelWidth + xOff + 8F + titleWidth);
-            drawRect(panelWidth + xOff, yOff, width - xOff, height - yOff, TITLE_BOX_COLOR);
-            drawRect(panelWidth + xOff, yOff, width - xOff, 2 * topBarBottom, MAIN_BOX_COLOR);
-            drawHorizontalLine(panelWidth + xOff, width - xOff, yOff, 0xff141414);
-            drawHorizontalLine(panelWidth + xOff, width - xOff, 2 * topBarBottom, 0xff141414);
-            drawHorizontalLine(panelWidth + xOff, width - xOff, height - yOff, 0xff141414);
+            //int titleBarX = (int) (panelWidth + xOff + 8F + titleWidth);
+
+            // Draw location / left panel
+            drawRect(panelWidth + xOff, yOff, mainPanelRight, height - yOff, TITLE_BOX_COLOR);
+            drawRect(panelWidth + xOff, yOff, mainPanelRight, 2 * topBarBottom, MAIN_BOX_COLOR);
+            // Outline
+            drawHorizontalLine(panelWidth + xOff, mainPanelRight, yOff, 0xff141414);
+            drawHorizontalLine(panelWidth + xOff, mainPanelRight, 2 * topBarBottom, 0xff141414);
+            drawHorizontalLine(panelWidth + xOff, mainPanelRight, height - yOff, 0xff141414);
             drawVerticalLine(panelWidth + xOff, yOff, height - yOff, 0xff141414);
+            drawVerticalLine(mainPanelRight, yOff, height - yOff, 0xff141414);
+
+            // Draw right panel
+            drawRect(mainPanelRight + xOff, yOff, width - xOff, height - yOff, TITLE_BOX_COLOR);
+            drawRect(mainPanelRight + xOff, yOff, width - xOff, topBarBottom + 1, MAIN_BOX_COLOR);
+            // Outline
+            drawHorizontalLine(mainPanelRight + xOff, width - xOff, yOff, 0xff141414);
+            drawHorizontalLine(mainPanelRight + xOff, width - xOff, topBarBottom, 0xff141414);
+            drawHorizontalLine(mainPanelRight + xOff, width - xOff, height - yOff, 0xff141414);
+            drawVerticalLine(mainPanelRight + xOff, yOff, height - yOff, 0xff141414);
             drawVerticalLine(width - xOff, yOff, height - yOff, 0xff141414);
 
-            drawRect(panelWidth + xOff + 1, yOff + 1, titleBarX, topBarBottom, 0xff626262);
-            // draw foreground
+
+            // Draw behind claim title
+            drawRect(panelWidth + xOff + 1, yOff + 1, mainPanelRight, topBarBottom, 0xff626262);
+
+
+            // Start draw foregrounds
+
+            // Draw claim title
             GlStateManager.pushMatrix();
             {
                 GlStateManager.scale(1.5F, 1.5F, 1.5F);
                 this.mc.fontRenderer.drawStringWithShadow(title, (panelWidth + xOff + 3) / 1.5F, yOff + 3, 0xffffff);
             }
             GlStateManager.popMatrix();
-            drawHorizontalLine(panelWidth + xOff + 1, titleBarX, topBarBottom - 1, 0xff141414);
-            drawVerticalLine(titleBarX, yOff, topBarBottom, 0xff141414);
+
+            // Draw line below title
+            drawHorizontalLine(panelWidth + xOff + 1, mainPanelRight, topBarBottom - 1, 0xff141414);
+
+            // Add tabs for right panel
             if(tabs == null) {
-                tabs = new GuiTabSelection(mc, titleBarX + 2, width - xOff, yOff + 1);
-                tabs.addButton(LOCATION_TAB_BUTTON_ID, "Location");
+                tabs = new GuiTabSelection(mc, mainPanelRight + xOff + 1, width - xOff, yOff + 1);
                 tabs.addButton(PERMISSION_TAB_BUTTON_ID, "Permissions");
                 tabs.addButton(GROUP_TAB_BUTTON_ID, "Groups");
             }
-            if(claimDeleteButton == null) {
-                claimDeleteButton = new GuiButton(2, titleBarX + 2, topBarBottom + 2, (width - xOff - titleBarX) / 3, 20, "Delete");
-                this.addButton(claimDeleteButton);
-            } else {
-                claimDeleteButton.setWidth((width - xOff - titleBarX) / 3);
-                claimDeleteButton.x = titleBarX + 2;
-                claimDeleteButton.drawButton(mc, mouseX, mouseY, partialTicks);
-            }
+
+            // Update if claim changes due to title sizing
             if(claim != lastClaim) {
-                tabs.setDimensions(titleBarX + 2, width - xOff, yOff + 1);
+                tabs.setDimensions(mainPanelRight + xOff + 1, width - xOff, yOff + 1);
             }
+            // Draw tabs
             tabs.drawScreen(mouseX, mouseY, partialTicks);
+
+            // Set up for drawing player name
             String name = claim.getOwner().toString();
             String oName = name;
             float scaleO = 0.8F;
+            int nameWidth = 0;
+
+            // Draw player name and fill nameWidth with the width in pixels
             GlStateManager.pushMatrix();
             {
                 GlStateManager.scale(scaleO, scaleO, scaleO);
@@ -121,30 +148,49 @@ public class ClaimListGUI extends GuiScreen {
                 if(name.length() != oName.length()) {
                     name += "...";
                 }
+                nameWidth = (int) (mc.fontRenderer.getStringWidth("Owner: " + name) * scaleO);
                 this.mc.fontRenderer.drawStringWithShadow("Owner: " + name, (panelWidth + xOff + 3) / scaleO, (topBarBottom + 3) / scaleO, 0xffffff);
             }
             GlStateManager.popMatrix();
-            if(tabs.getSelectedID() == LOCATION_TAB_BUTTON_ID) {
-                this.drawString(mc.fontRenderer, "Corners", panelWidth + xOff + 2, 4 + (2 * topBarBottom), 0xffffff);
-                BlockPos[] corners = claim.getFourCorners();
-                for(int i = 0; i < corners.length; i++) {
-                    this.drawString(mc.fontRenderer, corners[i].getX() + ", " + corners[i].getZ(), panelWidth + xOff + 2, (2 * topBarBottom) + 15 + (10 * i), 0xffffff);
-                }
-                this.drawString(mc.fontRenderer, "Dimension: " + claim.getDimensionID(), width - xOff - 2 - mc.fontRenderer.getStringWidth("Dimension:  " + claim.getDimensionID()), (2 * topBarBottom) + 4, 0xffffff);
-            } else if(tabs.getSelectedID() == PERMISSION_TAB_BUTTON_ID) {
-                this.drawString(mc.fontRenderer, "Permission", panelWidth + xOff + 2, 4 + (2 * topBarBottom), 0xffffff);
+
+            // Set up and draw delete button
+            if(claimDeleteButton == null) {
+                claimDeleteButton = new GuiButton(2, panelWidth + xOff + nameWidth + 10, topBarBottom + 2, mainPanelRight - (panelWidth + xOff + nameWidth + 10), 20, "Delete");
+                this.addButton(claimDeleteButton);
+            } else {
+                claimDeleteButton.setWidth(mainPanelRight - (panelWidth + xOff + nameWidth + 10));
+                claimDeleteButton.x = panelWidth + xOff + nameWidth + 10;
+                claimDeleteButton.drawButton(mc, mouseX, mouseY, partialTicks);
+            }
+
+            // Draw location information in left pane
+            this.drawString(mc.fontRenderer, "Corners", panelWidth + xOff + 2, 4 + (2 * topBarBottom), 0xffffff);
+            BlockPos[] corners = claim.getFourCorners();
+            for(int i = 0; i < corners.length; i++) {
+                this.drawString(mc.fontRenderer, corners[i].getX() + ", " + corners[i].getZ(), panelWidth + xOff + 2, (2 * topBarBottom) + 15 + (10 * i), 0xffffff);
+            }
+            this.drawString(mc.fontRenderer, "Dimension: " + claim.getDimensionID(), mainPanelRight - 2 - mc.fontRenderer.getStringWidth("Dimension:  " + claim.getDimensionID()), (2 * topBarBottom) + 4, 0xffffff);
+
+            // Draw permissions if selected
+            if(tabs.getSelectedID() == PERMISSION_TAB_BUTTON_ID) {
                 int i = 0;
                 for(UUID uuid : claim.getMembers().keySet()) {
-                    this.drawString(mc.fontRenderer, uuid.toString(), panelWidth + xOff + 2, (2 * topBarBottom) + 15 + (10 * i), 0xffffff);
+                    this.drawString(mc.fontRenderer, uuid.toString(), mainPanelRight + xOff + 2, (2 * topBarBottom) + 15 + (10 * i), 0xffffff);
                     i++;
                 }
+                if(i > 0) {
+                    this.drawString(mc.fontRenderer, "Permission", mainPanelRight + xOff + 2, 4 + (2 * topBarBottom), 0xffffff);
+                } else {
+                    this.drawString(mc.fontRenderer, "No Permissions Added", mainPanelRight + xOff + 2, 4 + (2 * topBarBottom), 0xffffff);
+                }
             }
-            
-            
-            if(mouseY >= (topBarBottom + 3) * scaleO && mouseY <= (topBarBottom + 3 + 20) * scaleO && mouseX >= (panelWidth + xOff + 3) && mouseX <= (panelWidth + xOff + 3 + titleWidth)) {
+
+            // Draw hover dialog for large player names
+            if(name.length() != oName.length() && mouseY >= (topBarBottom + 3) * scaleO && mouseY <= (topBarBottom + 3 + 20) * scaleO && mouseX >= (panelWidth + xOff + 3) && mouseX <= (panelWidth + xOff + 3 + titleWidth)) {
                 int width = mc.fontRenderer.getStringWidth("Owner: " + oName + "  ");
                 int left = mouseX - width;
                 int right = mouseX;
+                // Reverse the dialog if it doesn't fit on-screen
                 if(mouseX - width < 0) {
                     left = mouseX;
                     right = mouseX + width;
@@ -160,6 +206,8 @@ public class ClaimListGUI extends GuiScreen {
         } else {
             this.drawCenteredString(mc.fontRenderer, "No claim selected.", (width - (width / 3)), 15, 0xe3e3e3);
         }
+
+        // Draw result panel
         if(hasResult) {
             this.drawDefaultBackground();
             int boxWidth = 200;
@@ -173,10 +221,14 @@ public class ClaimListGUI extends GuiScreen {
             this.okPromptButton.y = (this.height / 2);
             this.okPromptButton.drawButton(mc, mouseX, mouseY, partialTicks);
         }
+
+        // Refresh after result pane is closed or opened
         if(lastHasResult ^ hasResult) {
             ClaimItGUI.NET.sendToServer(new CRefreshListPacket());
             this.resetClaimList();
         }
+
+        // Initialize next render
         lastHasResult = hasResult;
         lastClaim = claim;
     }
@@ -206,7 +258,7 @@ public class ClaimListGUI extends GuiScreen {
     public void onClaimRemoved(ClientClaimRemovedEvent event) {
         resetClaimList();
     }
-    
+
     public void resetClaimList() {
         panel.clearItems();
         for(ClaimArea claim : ClientClaimManager.getClaimsList()) {
@@ -237,7 +289,7 @@ public class ClaimListGUI extends GuiScreen {
                     panel.addItem(claim);
                 }
             }
-            if(panel != null && panel.getSelectedItem() != null && button == claimDeleteButton) {
+            if(panel != null && panel.getSelectedItem() != null && (button == claimDeleteButton || button.id == 2)) {
                 ClaimItGUI.NET.sendToServer(new CDeleteClaimPacket(panel.getSelectedItem()));
             }
         } else {
@@ -259,7 +311,7 @@ public class ClaimListGUI extends GuiScreen {
             tabs.handleMouseInput(i, j);
         }
     }
-    
+
     public String getPlayerName(UUID uuid) {
         NetworkPlayerInfo info = this.mc.player.connection.getPlayerInfo(uuid);
         return info.getDisplayName() != null ? info.getDisplayName().getFormattedText() : info.getGameProfile().getName();
